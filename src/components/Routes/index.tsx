174169e38ad62b4d061382488d1dc6782159c415
JohnOliver23 <johnoliver021@gmail.com>
2021-11-18 16:33:33 -0300

    modularizando libs

--- src/components/Routes/index.tsx
+++ src/components/Routes/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import SignIn from "../SignIn";
+import FormikModule from "../../pages/formik";
 
 const Routes: React.FC = () => (
   <Switch>
-    <Route path="/" exact component={SignIn} />
+    <Route path="/" exact component={FormikModule} />
   </Switch>
 );
 
---
+++ src/pages/formik/index.tsx
@@ -0,0 +1,7 @@
+import SignIn from "../../components/SignIn";
+
+const FormikModule: React.FC = () => {
+  return <SignIn />;
+};
+
+export default FormikModule;
